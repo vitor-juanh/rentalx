fc2305b355d27686592f6076cfa1d7b029497127
Vitor Juanh <vitor.juanh@gmail.com>
2023-09-08 18:04:45 -0400

    add support to json

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,8 @@ import { categoriesRoutes } from './routes/categories.routes';
 
 const app = express();
 
+app.use(express.json());
+
 app.use("/categories", categoriesRoutes);
 
 app.listen(3333, () => console.log('Server is running'));
