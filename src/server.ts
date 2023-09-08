88ae719ad0bbd116ec37bb4ab099872f2de065ed
Vitor Juanh <vitor.juanh@gmail.com>
2023-09-08 17:14:42 -0400

    add category route

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,8 @@
 import express from 'express';
+import { categoriesRoutes } from './routes/categories.routes';
 
 const app = express();
 
-app.get('/', (request, response) => {
-    return response.json({ message: 'Hello World' });
-});
+app.use(categoriesRoutes);
 
 app.listen(3333, () => console.log('Server is running'));
