170039b0dae3f2a143b86660c85d34b8ac83e038
Vitor Juanh <vitor.juanh@gmail.com>
2023-09-10 21:51:36 -0400

    create use case for create categories

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,24 +1,11 @@
 import { Router } from 'express';
-import { CreateCategoryService } from '../modules/cars/services/CreateCategoryService';
-import { CategoriesRepository } from '../modules/cars/repositories/CategoriesRepository';
+
+import { createCategoryController } from '../modules/cars/useCases/createCategory';
 
 const categoriesRoutes = Router();
-const categoriesRepository = new CategoriesRepository();
 
-categoriesRoutes.post('/', (request, response) => {
-    const { name, description } = request.body;
-
-    const createCategoryService = new CreateCategoryService(categoriesRepository);
-
-    createCategoryService.execute({ name, description });
-
-    return response.status(201).send();
-})
-
-categoriesRoutes.get('/', (request, response) => {
-    const all = categoriesRepository.index();
-
-    return response.json(all);
+categoriesRoutes.post('/', (request,response) => {
+    return createCategoryController.handle(request,response)
 })
 
 export { categoriesRoutes };
