9f8264dc49ebab87f1303c69d8c2ecb8a4f02b53
Vitor Juanh <vitor.juanh@gmail.com>
2023-09-18 19:40:39 -0400

    add implementations folder and use singleton

--- src/routes/specifications.routes.ts
+++ src/routes/specifications.routes.ts
@@ -1,7 +1,7 @@
 import { Router } from "express";
 
 import { CreateSpecificationService } from "../modules/cars/services/CreateSpecificationService";
-import { SpecificationsRepository } from "../modules/cars/repositories/SpecificationsRepository";
+import { SpecificationsRepository } from "../modules/cars/repositories/implementations/SpecificationsRepository";
 
 const specificationsRoutes = Router();
 
