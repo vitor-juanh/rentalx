b4d1c606eb8341e7e24bac80032972bd64160034
Vitor Juanh <vitor.juanh@gmail.com>
2023-09-18 21:08:45 -0400

    add swagger api docs

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,6 @@
 import express from 'express';
+import swaggerUi from 'swagger-ui-express';
+import swaggerFile from './swagger.json';
 
 import { router } from './routes';
 
@@ -7,5 +9,7 @@ const app = express();
 app.use(express.json());
 
 app.use(router);
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerFile))
 
 app.listen(3333, () => console.log('Server is running'));
